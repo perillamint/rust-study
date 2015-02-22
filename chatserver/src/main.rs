5ae8de544269f22a29899067f2a330d48c561e5d
KBuild <qwer7995@gmail.com>
2015-02-23 05:08:39 +0900

    Chat Server v0.1

--- chatserver/src/main.rs
+++ chatserver/src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 use std::thread::Thread;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Sender};
 use std::old_io::{TcpListener, TcpStream};
 use std::old_io::{Acceptor, Listener};
 use core::num::cast;
@@ -8,36 +8,57 @@ use core::num::cast;
 extern crate core;
 
 fn main() {
-	println!("Serer Start!");
+	println!("Server Start!");
 
-	let mut q = Arc::new(Mutex::new(Vec::new()));
+	// data Sender List
+	let mut txlist: Arc<Mutex<Vec<Sender<String>>>> = Arc::new(Mutex::new(Vec::new()));
 
-	let listener = TcpListener::bind("127.0.0.1:2345").unwrap();
+	let listener = TcpListener::bind("127.0.0.1:2345").unwrap(); //Server
 	let mut acceptor = listener.listen().unwrap();
 
 	for stream in acceptor.incoming() {
 		let strq = match stream {
-			Err(e) => { panic!("Connection Failed"); }
+			Err(e) => { panic!("Connection Failed. {:?}", e); }
 			Ok(stream) => {
-				Thread::spawn(move|| {
-					let mut stream = stream.clone();
+
+				let mut recv_stream = stream.clone();
+				let mut send_stream = stream.clone();
+
+				let read_txlist = txlist.clone();
+				let mut write_txlist = txlist.clone();
+				
+				Thread::spawn(move || { // read from stream 
 					println!("connected");
-					stream.write(String::from_str("Connected\n").as_bytes());
 					let mut datas = vec!();
 					loop {
 						let mut buf = [0];
-						let result = stream.read(&mut buf).unwrap();
+						let result = recv_stream.read(&mut buf).unwrap();
 						let mut datas = &mut datas;
-						if buf[0] == 13 || buf[0] == 10 {
-							println!("{}", String::from_utf8(datas.clone()).unwrap());
+						datas.push(buf[0]);
+						if buf[0] == 13 || buf[0] == 10 {						
+							//print!("{}", String::from_utf8(datas.clone()).unwrap()); //print to stdout
+							for mut tx in read_txlist.lock().unwrap().iter_mut() {
+								//print!("{}", &data);
+								let data = String::from_utf8(datas.clone());
+								if let Some(send_data) = data.ok() {
+									tx.send(send_data).unwrap();
+								}
+							}
 							datas.clear();
 						}
-						datas.push(buf[0]);
+					}
+				});
+				Thread::spawn(move || { // write to stream
+					send_stream.write(String::from_str("Connected\n").as_bytes());
+					let (tx, rx) = channel();
+					write_txlist.lock().unwrap().push(tx);
+					loop {
+						let data = rx.recv().unwrap();
+						send_stream.write(data.as_bytes());
 					}
 				});
 			}
 		};
-		q.lock().unwrap().push(strq);
 	}
 
 	drop(acceptor);
